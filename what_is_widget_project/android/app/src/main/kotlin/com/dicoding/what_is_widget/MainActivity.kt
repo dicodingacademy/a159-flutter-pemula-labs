db837d8a10efc8accc531324dd490be41cf6e5a7
Achmad Ilham <achmadilham07@gmail.com>
2022-07-27 17:10:16 +0700

    add what_is_widget_project

---
+++ what_is_widget_project/android/app/src/main/kotlin/com/dicoding/what_is_widget/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dicoding.what_is_widget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ what_is_widget_project/lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+/// Ini fungsi utama yang akan proyek Flutter jalankan.
+void main() {
+  runApp(const MyApp());
+}
+
+/// Kelas [MyApp] menjadi kelas utama saat menjalankan aplikasi
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    /// MaterialApp akan menampilkan aplikasi menggunakan Material Design
+    /// source: https://api.flutter.dev/flutter/material/MaterialApp-class.html
+    return MaterialApp(
+      /// Judul aplikasi Flutter dideklarasikan pada parameter [title]
+      title: 'Flutter Demo',
+
+      /// Tema aplikasi Flutter dideklarasikan pada parameter [theme]
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+
+      /// Parameter [home] adalah rute pertama yang akan dijalankan,
+      /// sedangkan widget [Scaffold] adalah tampilan dasar Material Design.
+      home: Scaffold(
+        /// Parameter [appBar] untuk menampilkan App Bar pada bagian atas [Scaffold].
+        appBar: AppBar(
+          title: const Text('Hello, world!'),
+        ),
+
+        /// Parameter [body] untuk menampilkan konten utama pada badan [Scaffold].
+        /// Kamu bisa mengubah isi konten aplikasi pada parameter ini.
+        /// 
+        /// Sebagian besar widget memiliki parameter [child], salah satunya adalah widget [Center]. 
+        /// Widget tersebut dianggap sebagai parent-widget, sedangkan widget pada parameter [child] 
+        /// dianggap sebagai child-widget. 
+        body: const Center(
+
+          /// Parameter [child] ini adalah sebuah "anak" dari widget [Center]. Kamu bisa memberikan 
+          /// satu widget apapun di dalamnya.
+          /// 
+          /// Berbeda jika ada widget yang memiliki parameter [children]. Parameter tersebut dapat 
+          /// kamu tambahkan dengan banyak anak. Kamu bisa temu pada widhet Row, Column, ListView, 
+          /// dan masih banyak lainnya.
+          child: Text('Hello, world!'),
+        ),
+      ),
+    );
+  }
+}
