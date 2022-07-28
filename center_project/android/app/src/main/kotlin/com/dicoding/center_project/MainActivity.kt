c99f2d5527c3a40dff9bd70e2fada62f59a49242
Achmad Ilham <achmadilham07@gmail.com>
2022-07-28 23:20:15 +0700

    add center_project

---
+++ center_project/android/app/src/main/kotlin/com/dicoding/center_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dicoding.center_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ center_project/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const FirstScreen(),
+    );
+  }
+}
+
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('First Screen'),
+      ),
+
+      /// Widget Center digunakan untuk membuat suatu widget
+      /// berada pada posisi tengah.
+      body: const Center(
+        child: Text('Text berada di tengah'),
+      ),
+    );
+  }
+}
