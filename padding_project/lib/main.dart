643bab4df6ec5acfdd0900637e51af4ae92df8ce
Achmad Ilham <achmadilham07@gmail.com>
2022-07-28 23:15:20 +0700

    add padding_project

---
+++ padding_project/lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const FirstScreen(),
+    );
+  }
+}
+
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('First Screen'),
+      ),
+
+      /// Kamu bisa gunakan widget [Container] untuk membuat
+      /// [padding]. Akan tetapi, widget [Container] ini merupakan
+      /// widget high-level yang memiliki parameter yang lengkap.
+      /// Apabila hanya ingin menampilkan padding antar widget,
+      /// kamu bisa gunakan widget [Padding].
+      ///
+      /// Widget [Padding] khusus untuk memberikan padding pada 
+      /// suatu widget.
+      body: const Padding(
+        padding: EdgeInsets.all(30),
+        child: Text('Ini Padding'),
+      ),
+    );
+  }
+}
