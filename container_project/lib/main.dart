5c0730f5bc1ee33352ad6eff3ba06cb86ad37167
Achmad Ilham <achmadilham07@gmail.com>
2022-07-28 23:21:49 +0700

    update comment

--- container_project/lib/main.dart
+++ container_project/lib/main.dart
@@ -29,20 +29,27 @@ class FirstScreen extends StatelessWidget {
         title: const Text('First Screen'),
       ),
       body: Container(
-        /// Kamu bisa beri warna background pada [Container] dengan parameter [color]. Akan tetapi, jika [Container] menerapkan parameter [decoration], parameter color harus dipindahkan ke [decoration].
+        /// Kamu bisa beri warna background pada [Container]
+        /// dengan parameter [color]. Akan tetapi, jika [Container]
+        /// menerapkan parameter [decoration], parameter color
+        /// harus dipindahkan ke [decoration].
         // color: Colors.blue,
 
         /// Kamu bisa mengatur lebar dan tinggi dari [Container].
         width: 200,
         height: 100,
 
-        /// Tambahkan [padding] untuk memberi jarak antara konten dengan [Container].
+        /// Tambahkan [padding] untuk memberi jarak antara konten
+        /// dengan [Container].
         padding: const EdgeInsets.all(10),
 
-        /// Tambahkan [margin] untuk memberi jarak antara [Container] dengan bagian luar [Container].
+        /// Tambahkan [margin] untuk memberi jarak antara [Container]
+        /// dengan bagian luar [Container].
         margin: const EdgeInsets.all(10),
 
-        /// Parameter [decoration] untuk melakukan styling. Kamu bisa menambahkan warna background, shadow, border, border-radius, shape, dan lain-lain.
+        /// Parameter [decoration] untuk melakukan styling. Kamu bisa
+        /// menambahkan warna background, shadow, border, border-radius,
+        /// shape, dan lain-lain.
         decoration: BoxDecoration(
           color: Colors.red,
           // shape: BoxShape.circle,
@@ -57,7 +64,8 @@ class FirstScreen extends StatelessWidget {
           borderRadius: BorderRadius.circular(10),
         ),
 
-        /// Teks 'Hi' akan dibungkus dengan [Container] dengan variasi bentuk, styling dan layout.
+        /// Teks 'Hi' akan dibungkus dengan [Container] dengan variasi
+        /// bentuk, styling dan layout.
         child: const Text(
           'Hi',
           style: TextStyle(fontSize: 40),
