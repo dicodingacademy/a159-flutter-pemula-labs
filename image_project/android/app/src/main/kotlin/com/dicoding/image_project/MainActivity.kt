7dfff3d203c77fe3709a191e210d8f4f0c43b864
Achmad Ilham <achmadilham07@gmail.com>
2022-07-29 10:33:43 +0700

    add image_project

---
+++ image_project/android/app/src/main/kotlin/com/dicoding/image_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dicoding.image_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ image_project/lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const FirstScreen(),
+    );
+  }
+}
+
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('First Screen'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.network(
+              'https://picsum.photos/200/300',
+              width: 200,
+              height: 200,
+            ),
+            const SizedBox(height: 30),
+            Image.asset(
+              "assets/android.png",
+              width: 200,
+              height: 200,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
